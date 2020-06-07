10c8efc01df298398f327edc6a7c02ad9eb8b6b7
Alexandre Metgy <alexandre.metgy@gmail.com>
2020-06-07 12:45:43 +0200

    Add DbContext and Question data model.

---
+++ QAEngine.Api/Data/Context/QAEngineContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QAEngine.Api.Data
+{
+    public class QAEngineContext : DbContext
+    {
+        public QAEngineContext(DbContextOptions<QAEngineContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
---
+++ QAEngine.Api/Data/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace QAEngine.Api.Data
+{
+    public class Question
+    {
+        public string Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTimeOffset CreateDate { get; set; }
+    }
+}
