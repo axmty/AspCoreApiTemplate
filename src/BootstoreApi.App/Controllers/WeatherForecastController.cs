416e2501253521994db53962f16f6928ce575909
Alexandre Metgy <alexandre.metgy@gmail.com>
2021-03-20 00:05:32 +0100

    Add .editorconfig with code style rules.

--- src/BootstoreApi.App/Controllers/WeatherForecastController.cs
+++ src/BootstoreApi.App/Controllers/WeatherForecastController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace BootstoreApi.App.Controllers
 {
@@ -11,18 +10,21 @@ namespace BootstoreApi.App.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private static readonly int Er = 2;
+        private readonly int rrr = 2;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> log)
         {
-            _logger = logger;
+            logger = log;
         }
-
+        
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
